1ff09f3a11cd3d35d168c0186ce74eb466ec89bd
张新永 <xinyong.zhang@zen-x.com.cn>
2023-12-15 19:12:19 +0800

    fix: 1

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -7,11 +7,11 @@ import internal, { pipeline } from "stream"
 
 const URL = "http://127.0.0.1:8000/user/upload/avatar/"
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-}
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// }
 
 export async function GET(): Promise<Response> {
   return new Response("GET Draft mode is disabled")
@@ -32,8 +32,6 @@ export async function POST(request: Request) {
   //   // 使用stream.pipeline将文件内容流式传输到文件系统
   //   await streamPipeline(file.stream() as any, writableStream)
   // }
-  const name = formData.get("name")
-  const email = formData.get("email")
   const data = await fetch(URL, {
     method: "POST",
     body: formData,
