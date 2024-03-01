6c9981cf55847863db1375ebf006d73d192696f3
张新永 <xinyong.zhang@zen-x.com.cn>
2024-03-01 16:09:51 +0800

    feat: 处理错误

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -4,12 +4,11 @@ import Add from "./add"
 import Delete from "./delete"
 
 const About = () => {
-  const { count, date } = useStore((state) => state)
+  const { count } = useStore((state) => state)
 
   return (
     <div className="p-4">
       <div>当前count:{count}</div>
-      <div>当前date:{date}</div>
       <div className="flex gap-1 flex-wrap mb-1 ">
         <Add />
         <Delete />
--- src/components/User/index.tsx
+++ src/components/User/index.tsx
@@ -1,7 +1,7 @@
 import { sql } from "@vercel/postgres"
 
 export default async function User() {
-  const { rows } = await sql`SELECT * from CARTS`
+  const { rows } = await sql`SELECT * from User `
 
   console.log(rows)
 
@@ -10,8 +10,8 @@ export default async function User() {
       <div>
         当前用户列表
         <ul>
-          {rows.map((user) => (
-            <li key={user.id}>{user.name}</li>
+          {rows.map((user, index) => (
+            <li key={user.id + index}>{user.user + "_" + index}</li>
           ))}
         </ul>
       </div>
