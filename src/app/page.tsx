475893cab5ccfd1852593aae085560d8476d81be
张新永 <xinyong.zhang@zen-x.com.cn>
2024-03-01 16:29:12 +0800

    feat: test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import User from "@/components/User"
 export default function Home() {
   return (
     <main>
+      <span>检测到变化就会自动构建的吗？？？？</span>
       {/* <HomeForm></HomeForm> */}
       <StreamList></StreamList>
       <h2>Zustand 状态管理</h2>
